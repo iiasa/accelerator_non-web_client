fba58dfabe0f5190b3f659451e6270dc4a6abad3
Rupesh Singh <wrufesh@gmail.com>
2023-09-05 17:13:04 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name='acli',
+    version='1.0',
+    description='IIASA Accelerator python client',
+    author='Wrufesh S',
+    author_email='wrufesh@gmail.com',
+    py_modules=['acli'],
+)
